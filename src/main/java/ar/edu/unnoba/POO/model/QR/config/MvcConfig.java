7849e39954812d97606ca0fa51a41f43890657b7
GFioriUNNOBA <fiorigabriel7@gmail.com>
2022-11-11 16:33:11 -0300

    se agrega el controler de admin y el html de index, new e index

---
+++ src/main/java/ar/edu/unnoba/POO/model/QR/config/MvcConfig.java
@@ -0,0 +1,16 @@
+package ar.edu.unnoba.POO.model.QR.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by jpgm on 31/10/22.
+ */
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+    }
+}
--- src/main/java/ar/edu/unnoba/POO/model/QR/controller/AdminController.java
+++ src/main/java/ar/edu/unnoba/POO/model/QR/controller/AdminController.java
@@ -1,12 +1,47 @@
 package ar.edu.unnoba.POO.model.QR.controller;
 
 import ar.edu.unnoba.POO.model.QR.model.Adminitrador;
-import org.springframework.security.core.userdetails.User;
+import ar.edu.unnoba.POO.model.QR.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-import java.net.Authenticator;
+import java.util.List;
 
+@Controller
+@RequestMapping("/admin")
 public class AdminController {
+    private IUserService userService;
+    @Autowired
+    public AdminController(IUserService userService){
+        this.userService= userService;
+    }
+    @GetMapping
+    public String index(Model model){
+        List<Adminitrador> users = userService.getAll();
+        model.addAttribute("admin",users);
+        return "admin/index";
+    }
 
+    @GetMapping("/new")
+    public String userNew(Model model){
+        model.addAttribute("admin",new Adminitrador());
+        return "admin/new";
+    }
+
+
+    @PostMapping
+    public String create(@ModelAttribute Adminitrador user){
+        userService.create(user);
+        return "redirect:/admin/new";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable("id") Long id){
+        userService.delete(id);
+        return "redirect:/admin/";
+    }
 // metodo  public Admin (Authentication authentication)
 
 }
